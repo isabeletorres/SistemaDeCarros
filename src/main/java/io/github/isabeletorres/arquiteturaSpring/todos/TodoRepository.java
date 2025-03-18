b7d29005ed93159c472a32bd78c4b9527927f69d
Isabele Torres <isabeledasilvatorres@gmail.com>
2025-03-18 15:25:55 -0300

    Trabalhando com Components

--- src/main/java/io/github/isabeletorres/arquiteturaSpring/todos/TodoRepository.java
+++ src/main/java/io/github/isabeletorres/arquiteturaSpring/todos/TodoRepository.java
@@ -1,6 +1,9 @@
 package io.github.isabeletorres.arquiteturaSpring.todos;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.*;
 
+@Repository
 public interface TodoRepository extends JpaRepository<TodoEntity, Integer>{
+    boolean existsByDescricao(String descricao);
 }
--- src/main/java/io/github/isabeletorres/arquiteturaSpring/todos/TodoService.java
+++ src/main/java/io/github/isabeletorres/arquiteturaSpring/todos/TodoService.java
@@ -5,17 +5,28 @@ import org.springframework.stereotype.*;
 @Service
 public class TodoService {
 
-    private TodoRepository repository;
 
-    public TodoService(TodoRepository todoRepository){
+    private TodoRepository repository;
+    private TodoValidator validator;
+    private MailSender mailSender;
+
+    public TodoService(TodoRepository todoRepository,
+                       TodoValidator validator,
+                       MailSender mailSender){
         this.repository = todoRepository;
+        this.validator = validator;
+        this.mailSender = mailSender;
     }
+
     public TodoEntity salvar(TodoEntity novoTodo){
+        validator.validar(novoTodo);
         return repository.save(novoTodo);
     }
 
     public void atualizarStatus(TodoEntity todo){
         repository.save(todo);
+        String status = todo.getConcluido() == Boolean.TRUE ? "Concluído" : "Não conclído";
+        mailSender.enviar("Todo de código " + todo.getDescricao() + " foi atualizado para " + status);
     }
     public TodoEntity buscarId(Integer id){
         return repository.findById(id).orElse(null);
